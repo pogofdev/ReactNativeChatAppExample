c36a06f0199478110d66166f9c82e988e40d415a
poisongas <poisongass@hotmail.com>
2018-03-18 22:16:27 +0700

    added onesignal lib

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 import {StackNavigator} from 'react-navigation';
 import room from "./client/room";
 import index from "./client/index";
+import OneSignal from "react-native-onesignal";
 
 
 const Rootstack = StackNavigator(
@@ -31,4 +32,43 @@ const Rootstack = StackNavigator(
     }
 );
 
-export default Rootstack
+export default class App extends Component {
+    componentWillMount() {
+        OneSignal.addEventListener('received', this.onReceived);
+        OneSignal.addEventListener('opened', this.onOpened);
+        // OneSignal.addEventListener('registered', this.onRegistered);
+        OneSignal.addEventListener('ids', this.onIds);
+        // Example, always display notification in shade. android only
+        OneSignal.inFocusDisplaying(0);
+    }
+
+    componentWillUnmount() {
+        OneSignal.removeEventListener('received', this.onReceived);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        // OneSignal.removeEventListener('registered', this.onRegistered);
+        OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived(notification) {
+        console.log("Notification received: ", notification);
+    }
+
+    onOpened(openResult) {
+        console.log('Message: ', openResult.notification.payload.body);
+        console.log('Data: ', openResult.notification.payload.additionalData);
+        console.log('isActive: ', openResult.notification.isAppInFocus);
+        console.log('openResult: ', openResult);
+    }
+
+    onRegistered(notifData) {
+        console.log("Device had been registered for push notifications!", notifData);
+    }
+
+    onIds(device) {
+        console.log('Device info: ', device);
+    }
+    render(){
+        return(<Rootstack/>)
+    }
+}
+
